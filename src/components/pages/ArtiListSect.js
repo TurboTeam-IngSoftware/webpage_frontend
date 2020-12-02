2fab7b1753cbd3af0b9b7b69e95161d4f4c8d409
jordiugarte <jordi933@gmail.com>
2020-12-02 10:25:16 -0400

    lista

--- src/components/pages/ArtiListSect.js
+++ src/components/pages/ArtiListSect.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
