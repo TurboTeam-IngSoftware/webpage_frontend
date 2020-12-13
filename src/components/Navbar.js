7580e62740fe06e8eb9524f4ae499dd253057f32
0WLB34R <knhrvs@gmail.com>
2020-12-13 15:03:24 -0400

    Un Test vara ver la funcionalidad de webhook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ function Navbar() {
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
               <li className='nav-item'>
                 <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                  Home
+                  Home Test
                 </Link>
               </li>
               <li className='nav-item'>
