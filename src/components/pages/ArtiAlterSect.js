d39f1ff0256dc1053de77e3479d4c8167cd3149a
jordiugarte <jordi933@gmail.com>
2020-12-16 08:37:51 -0400

    Actualizacion

--- src/components/pages/ArtiAlterSect.js
+++ src/components/pages/ArtiAlterSect.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import {TextField} from '@material-ui/core'
 import { useLocation } from "react-router-dom";
 import Axios from 'axios'
-
+import Typography from '@material-ui/core/Typography';
 
 function ArtiAlterSect() {
     
@@ -15,11 +15,24 @@ function ArtiAlterSect() {
     const [content, setContent] = useState(post.description);
     const [video, setVideo] = useState(post.video);
     const [descripcorta, setDescripCorta] = useState(post.shortDescription);
+    const [imagen, setImagen] = useState("");
     const today = new Date();
 
-    const publish = () => {
+    const validTitle = title.length >= 3;
+    const validateShortDescription = descripcorta.length >= 30 && descripcorta.length <= 100;
+    const validateContent = content.length >= 500 && content.length <= 3000;
 
-        Axios.put("http://skynet.lp.upb.edu/~pbruckner18/webpage_backend/posts", {
+    const uploadImage = () => {
+        const fd = new FormData();
+        fd.append('image', imagen);
+        Axios.post("webpage_backend/photos", fd)
+        .then(res => {
+            console.log(res);
+        })
+    }
+
+    const update = () => {
+        Axios.put("webpage_backend/posts", {
             idPost: post.idPost,
             title: title,
             shortDescription: descripcorta,
@@ -30,9 +43,10 @@ function ArtiAlterSect() {
             category: "economia",
             revised: "0",
             video: "https://www.youtube.com/embed/"+video.split("=")[1],
-    }).then((response) => {
-        console.log(response);
-    });
+        }).then((response) => {
+            console.log(response);
+        });
+        
     };
     const isLogged = localStorage.getItem('role') !== "";
 
@@ -40,63 +54,92 @@ function ArtiAlterSect() {
         <div>
         {localStorage.getItem("role") === "1" || localStorage.getItem("role") === "2" ? 
         <div className='containerArtiEdit'>
-        <h1 className='title'>
-            Editando Artículo
-        </h1>
+        <Typography component="h1" variant="h5">
+            Edición de artículo
+        </Typography>         
         <TextField
-            type='text'
-            defaultValue={post.title}
+            variant="outlined"
+            margin="normal"
+            required
+            defaultValue={title}
+            id="title"
             label="Título"
-            color="primary"
-            variant="filled"
+            name="email"
+            autoComplete="title"
+            autoFocus
             onChange={(e) => {
                 setTitle(e.target.value);
             }}
-            />
-        <TextField
-        type='text'
-        label="Descripción Corta"
-        defaultValue={post.shortDescription}
-        color="primary"
-        variant="filled"
-        onChange={(e) => {
-            setDescripCorta(e.target.value);
-        }}
+            helperText={validTitle ? "Título válido" : "Título inválido"}
+            error={!validTitle}
         />
-        <h1 className='content'>
-            Contenido
-        </h1>
         <TextField
-          id="standard-textarea"
-          label="Contenido"
-          placeholder="Contenido"
-          defaultValue={post.description}
-          multiline
-          fullWidth
-          onChange={(e) => {
-            setContent(e.target.value);
-        }}
+            variant="outlined"
+            margin="normal"
+            required
+            defaultValue={descripcorta}
+            id="short"
+            label="Descripción corta"
+            name="email"
+            autoComplete="short"
+            autoFocus
+            onChange={(e) => {
+                setDescripCorta(e.target.value);
+            }}
+            helperText={validateShortDescription ? "Descripción válida" : "La descripción debe ser de 30 a 100 caracteres."}
+            error={!validateShortDescription}
         />
-        <h1 className='extras'>
-            Extras
-        </h1>
         <TextField
-            type='text'
-            label="Link de video"
-            defaultValue={post.video}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            defaultValue={content}
+            id="content"
+            label="Contenido"
+            name="content"
+            autoComplete="content"
+            autoFocus
+            multiline
+            rows={20}
+            rowsMax={40}
+            onChange={(e) => {
+                setContent(e.target.value);
+            }}
+            helperText={validateContent ? "" : "El contenido debe tener de 500 a 300 caracteres."}
+            error={!validateContent}
+        />
+        <TextField
+            variant='outlined'
+            margin='normal'
+            fullWidth
+            id='video'
+            type='url'
+            label="Link de video (opcional)"
             color="primary"
-            variant="filled"
             height="100%"
             onChange={(e) => {
                 setVideo(e.target.value);
-                console.log(video.split("=")[1]);
             }}
+            />
+        <input 
+        
+        type="file"
+        onChange={(e)=>{
+            setImagen(e.target.files[0])
+        }} 
+        //ref={fileInput => this.fileInput = fileInput}
+        />
+        <ButtonComp 
+            text={'Subir imagen'}
+            disabled={false}
+            onClick={uploadImage}
             />
         <Link to ='/artireded' >
             <ButtonComp 
                 text={'Actualizar'}
                 disabled={false}
-                onClick={publish}
+                onClick={update}
              />
         </Link>
     </div>
